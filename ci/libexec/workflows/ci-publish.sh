5720be0e73d529a56a60b42be442cd9263a38ebf
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2022-10-16 18:10:10 -0500

    fix(build): fix nuget push globbing error on publish

--- ci/libexec/workflows/ci-publish.sh
+++ ci/libexec/workflows/ci-publish.sh
@@ -19,9 +19,14 @@ set -o pipefail # Fail pipelines if any command errors, not just the last one.
 function ci-publish() {
   printf "Publishing composed artifacts...\n\n"
 
-  ci-dotnet-nuget-push
+  local nugetOutput="${BUILD_PACKAGED_DIST}/nuget"
 
-  printf "Publishing complete.\n\n"
+  # dotnet nuget push supports wildcard package names. See: https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-nuget-push
+  printf "\n  Pushing NuGet packages to '%s' using 'dotnet nuget push'.\n" "${NUGET_SOURCE}" &&
+    cd "${nugetOutput}" &&
+    dotnet nuget push "*.nupkg" --api-key "${NUGET_API_KEY}" --source "${NUGET_SOURCE}" &&
+    printf "\n  Pushed NuGet packages to '%s'.\n" "${NUGET_SOURCE}" &&
+    printf "Publishing complete.\n\n"
 }
 
 export -f ci-publish
