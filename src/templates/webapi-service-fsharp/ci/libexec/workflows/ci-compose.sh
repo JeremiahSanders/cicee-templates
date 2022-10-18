1fe66f9fe3277280e89c8bf86036deb93fb94f88
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2022-10-17 22:45:39 -0500

    task: update template validate and compose

--- src/templates/webapi-service-fsharp/ci/libexec/workflows/ci-compose.sh
+++ src/templates/webapi-service-fsharp/ci/libexec/workflows/ci-compose.sh
@@ -8,8 +8,8 @@
 #   This script expects a CICEE CI library environment (which is provided when using 'cicee lib exec').
 #   For CI library environment details, see: https://github.com/JeremiahSanders/cicee/blob/main/docs/use/ci-library.md
 #
-# How to use:
-#   Modify the "ci-compose" function, below, to execute the steps required to produce the project's artifacts.
+#   Workflow:
+#   - Publish an executable from src/webapi using 'Release' configuration.
 ###
 
 set -o errexit  # Fail or exit immediately if there is an error.
@@ -17,17 +17,13 @@ set -o nounset  # Fail if an unset variable is used.
 set -o pipefail # Fail pipelines if any command errors, not just the last one.
 
 function ci-compose() {
-  printf "Composing build artifacts...\n\n"
-
-  # Build an executable
-  dotnet publish "${PROJECT_ROOT}/src/webapi" \
-    --configuration Release \
-    --output "${BUILD_UNPACKAGED_DIST}" \
-    -p:Version="${PROJECT_VERSION_DIST}"
-
-  printf "\nWebApi executable published to %s\n\n" "${BUILD_UNPACKAGED_DIST}"
-
-  printf "Composition complete.\n"
+  printf "Composing build artifacts...\n\n" &&
+    dotnet publish "${PROJECT_ROOT}/src/webapi" \
+      --configuration Release \
+      --output "${BUILD_UNPACKAGED_DIST}" \
+      -p:Version="${PROJECT_VERSION_DIST}" &&
+    printf "\nWebApi executable published to %s\n\n" "${BUILD_UNPACKAGED_DIST}" &&
+    printf "Composition complete.\n"
 }
 
 export -f ci-compose
